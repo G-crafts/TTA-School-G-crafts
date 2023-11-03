72e5d4fe030b3085be5b53a02fc9ce8fbe78117a
G-crafts <katelynn.graham@malad.us>
2023-11-02 23:46:19 -0600

    Horse1.java
    Original Horse Class with added objects/arraylist. Printed out. New sort method and printed out sort method.

---
+++ Horse1.java
@@ -0,0 +1,66 @@
+//katelynn.graham@malad.us
+
+//Simple horse class by Katelynn Graham/G-crafts
+
+//for CTE Software Development class 2023
+
+import java.util.Arrays;
+import java.util.Collections.sort;
+public class Horse1 implements Comparable{
+
+
+    public interface Comparable<a>;
+
+
+    public Horse(String horseName, int year){
+
+        String name=horseName;
+        birthYear=year;
+
+    }
+    void public changeName(String newName){
+        name=newName;
+    
+    String public toString(){
+        return name;
+        
+        Horse horse1= new Horse("dolly",2008);
+        Horse horse2= new Horse("annie",1990);
+        Horse horse3= new Horse("fabio",2004);
+
+            System.out.println("dolly");
+            System.out.println("annie");
+            System.out.println("fabio");
+            
+    }
+// Add the compareTo method
+    {public String Horse.compareTo(A o);
+    return.this horseName-birthYear
+
+    String name;
+    
+    int birthYear;
+    //Add the rest of the horses
+    { 
+        Horse horse4= new Horse("Alice",2009);
+        Horse horse5= new Horse("Reddie",2012);
+        Horse horse6= new Horse("Loppy", 2014);
+        Horse horse7= new Horse("Stubble", 2019);
+        Horse horse8= new Horse("George", 2022);
+        Horse horse9= new Horse("Yally", 2003);
+        Horse horse10=new Horse("Fred", 2005);
+//To print the rest of the horses
+        System.out.print(horse4, horse5, horse6, horse7, horse8, horse9, horse10);
+
+    
+    }
+    //Use the java.util.collections.sort() method
+    Collections.sort();{
+    Object ArrayList = (birthYear-1-10); 
+    String Object-{ArrayList = (horseName-1-10); 
+// Print out the objects again
+    System.out.print(horse4, horse5, horse6, horse7, horse8, horse9, horse10);
+
+        }
+}
+    }
