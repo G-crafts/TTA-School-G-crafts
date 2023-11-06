38dc75fc120f0fbb83b08aa5a047c045b3508f60
G-crafts <katelynn.graham@malad.us>
2023-11-05 21:40:22 -0700

    ExceptionsErrorProject.java
    G-crafts project
    KG
    SD1
    The exceptions programming project
    11/5/23

---
+++ ExceptionsErrorProject.java
@@ -0,0 +1,55 @@
+//katelynn.graham@malad.us
+//Katelynn Graham
+//Software and Development Semester 1
+//Exceptions Programming Project
+
+public class ExceptionsErrorProject{
+    public static void main(String[] args){
+        int arr[]=new int[7];
+        //creating a integer origin statement
+        int[] myIntArray = new int[6/2];
+    } {
+    //lets make some integer statements!
+     num [0] = new int[15/5];
+    num [1] = new int[4/0];
+    //The int statement with an exception
+    num [2] = new String (34/0);
+    // lets add the try statement
+    try
+{
+    System.out.println(num [2]);
+    
+}
+
+//handling the exception
+    catch(ArithmeticException e)
+{
+    System.out.println(e);
+}
+{
+    System.out.println("rest of the code can continue without the program crashing");
+}
+    //Continuing the string list after the exception
+    num [3] = new int[18/9];
+    //Adding another exception
+    num [4] = new int[24/0];
+    //Adding another try/catch statement
+    try
+    {
+        System.out.println(num [4]);
+    }
+    //handling the exception with another catch statement
+    catch(ArithmeticException e)
+    {
+        System.out.println(e);
+    }
+    {
+        System.out.println("rest of the code can continue without the program crashing");
+    }
+    //Finishing the array list of integers division
+    num [5] = new int[100/10];
+    num [6] = new int[14/7];
+    //printing out the array
+    System.out.print(int[].class);
+     }
+}
