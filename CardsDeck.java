0dbefa2929c60ed94e60a953a71161fee34da2fe
G-crafts <katelynn.graham@malad.us>
2023-12-01 13:37:41 -0700

    Cards Project CardsDeck Class and Deck Class
    CardsDeck is the cards class with the descriptions, the card deck, labels, suits, suit types, hands dealt, cards dealt per hand, and the number of hands dealt. Deck is the child class with the parameters of the parent class and will be the class that shuffles and deals the deck.

---
+++ CardsDeck.java
@@ -0,0 +1,108 @@
+//katelynn.graham@malad.us
+//Katelynn Graham
+//G-crafts
+//Mr. Gross
+//Cards Project Card Class
+//Software and Programming Development 11A
+//December 1, 2023
+public class CardsDeck {
+    public static void main (String[] args) 
+     {
+        //Create the class variables
+        String cardType = cardSuit;
+        int cardSuitNumber = numberOfSuits;
+        int cards = cardsInDeck;
+        int cardsSuit = numberOfCardsInSuit;
+        String suitType = typeOfCardsInSuit;
+        String typeofCard = typeOfCardInDeck;
+        int cardsDealt = numberOfCardsDealtOverall;
+        int cardsDealtPerHand = numberOfCardsDeltToEachPersonPerHand;
+        int numberOfHands = numberOfHandsDealt;
+        }
+    //create the cards
+    {
+        Card cardInfo1 = new int cardSuitNumber [4];
+        //how many suits there are
+        Card cardInfo2 = new int cardsSuit [13];
+        // how many cards are in a suit
+        Card cardInfo3 = new suitType ("Spades");
+        Card cardInfo3 = new suitType ("Clubs");
+        Card cardInfo3 = new suitType ("Hearts");
+        Card cardInfo3 = new suitType ("Diamonds");
+        //the different suits
+        Card cardInfo4 = new int cards [54];
+        //number of cards in the deck
+        Card cardInfo5 = new typeOfCard ("Ace of Spades");
+        Card cardInfo5 = new typeOfCard ("King of Spades");
+        Card cardInfo5 = new typeOfCard ("Queen of Spades");
+        Card cardInfo5 = new typeOfCard ("Jack of Spades");
+        Card cardInfo5 = new typeOfCard ("2 of Spades");
+        Card cardInfo5 = new typeOfCard ("3 of Spades");
+        Card cardInfo5 = new typeOfCard ("4 of Spades");
+        Card cardInfo5 = new typeOfCard ("5 of Spades");
+        Card cardInfo5 = new typeOfCard ("6 of Spades");
+        Card cardInfo5 = new typeOfCard ("7 of Spades");
+        Card cardInfo5 = new typeOfCard ("8 of Spades");
+        Card cardInfo5 = new typeOfCard ("9 of Spades");
+        Card cardInfo5 = new typeOfCard ("10 of Spades");
+// the cards in the spades suit
+        Card cardInfo5 = new typeOfCard ("Ace of Hearts");
+        Card cardInfo5 = new typeOfCard ("King of Hearts");
+        Card cardInfo5 = new typeOfCard ("Queen of Hearts");
+        Card cardInfo5 = new typeOfCard ("Jack of Hearts");
+        Card cardInfo5 = new typeOfCard ("2 of Hearts");
+        Card cardInfo5 = new typeOfCard ("3 of Hearts");
+        Card cardInfo5 = new typeOfCard ("4 of Hearts");
+        Card cardInfo5 = new typeOfCard ("5 of Hearts");
+        Card cardInfo5 = new typeOfCard ("6 of Hearts");
+        Card cardInfo5 = new typeOfCard ("7 of Hearts");
+        Card cardInfo5 = new typeOfCard ("8 of Hearts");
+        Card cardInfo5 = new typeOfCard ("9 of Hearts");
+        Card cardInfo5 = new typeOfCard ("10 of Hearts");
+        // cards in the hearts suit
+        Card cardInfo5 = new typeOfCard ("Ace of Diamonds");
+        Card cardInfo5 = new typeOfCard ("King of Diamonds");
+        Card cardInfo5 = new typeOfCard ("Queen of Diamonds");
+        Card cardInfo5 = new typeOfCard ("Jack of Diamonds");
+        Card cardInfo5 = new typeOfCard ("2 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("3 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("4 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("5 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("6 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("7 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("8 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("9 of Diamonds");
+        Card cardInfo5 = new typeOfCard ("10 of Diamonds");
+        // the cards in diamonds suit
+        Card cardInfo5 = new typeOfCard ("Ace of Clubs");
+        Card cardInfo5 = new typeOfCard ("King of Clubs");
+        Card cardInfo5 = new typeOfCard ("Queen of Clubs");
+        Card cardInfo5 = new typeOfCard ("Jack of Clubs");
+        Card cardInfo5 = new typeOfCard ("2 of Clubs");
+        Card cardInfo5 = new typeOfCard ("3 of Clubs");
+        Card cardInfo5 = new typeOfCard ("4 of Clubs");
+        Card cardInfo5 = new typeOfCard ("5 of Clubs");
+        Card cardInfo5 = new typeOfCard ("6 of Clubs");
+        Card cardInfo5 = new typeOfCard ("7 of Clubs");
+        Card cardInfo5 = new typeOfCard ("8 of Clubs");
+        Card cardInfo5 = new typeOfCard ("9 of Clubs");
+        Card cardInfo5 = new typeOfCard ("10 of Clubs");
+// cards in clubs suit
+        Card cardInfo6 = new int cardsDealt [30];
+        //cards dealt overall in all 5 hands
+        Card cardInfo7 = new int cardsDealtPerHand [6];
+        Card cartInfo8 = new int numberOfHands [5];
+    }
+        {
+    //Now let's print each section out
+    System.out.println (cardInfo1);
+    System.out.println  (cardInfo2);
+    System.out.print (cardInfo3);
+    System.out.println (cardInfo4);
+    System.out.print (cardInfo5);
+    System.out.println (cardInfo6);
+    System.out.println (cardInfo7);
+    System.out.println (cardInfo8);
+        }
+        //Let's inherit the deck into another class to make it shuffle the different types of cards
+}
