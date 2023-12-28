92a0a84d2fbe3b123074710dc111cf626e95ff9a
G-crafts <katelynn.graham@malad.us>
2023-12-28 13:53:15 -0700

    added edits

--- Dealer.java
+++ Dealer.java
@@ -5,22 +5,74 @@
 //Software and Programming dev
 //Mr Gross
 
-public class Dealer {
-    private Deck deck;
+import java.util.ArrayList;
 
-    public Dealer (){
-        this.deck = new Deck();
-        this.deck.shuffle();
-    }
-    //Method to dealing the cards to players
-    public void dealCards(Player player, int num cards) {
-        for (int i = 0 ; i < num cards; i++){
-            Card dealtCard = deck.dealCard();
-            if (dealtCard != null) {
-                player.recieveCard(dealtCard);
-            } else {
-                System.out.println("Deck is gone");
-            }
+public class Dealer {
+    Deck deckInUse;
+    int player1Score=0;
+    int player2Score=0;
+    int player3Score=0;
+    int player4Score=0;
+    int player5Score=0;
+
+public static void main (String[] args){
+    deckInUse=new Deck();
+    ArrayList player1=new ArrayList<>();
+    ArrayList player2=new ArrayList<>();
+    ArrayList player3=new ArrayList<>();
+    ArrayList player4=new ArrayList<>();
+    ArrayList player5=new ArrayList<>();
+    deckInUse.DeckShuffle();
+    for (int x=0;x<6;x++){
+        if (deckInUse.isNextCard){
+            player1.add(deckInUse.dealNextCard());
         }
+        if (deckInUse.isNextCard){
+            player2.add(deckInUse.dealNextCard());
+        }
+        if (deckInUse.isNextCard){
+            player3.add(deckInUse.dealNextCard());
+        }
+        if (deckInUse.isNextCard){
+            player4.add(deckInUse.dealNextCard());
+        }
+        if (deckInUse.isNextCard){
+            player5.add(deckInUse.dealNextCard());
+        }
+        System.out.println("player1's hand is");
+        for (int x=0; x<6;x++){
+            System.out.println(player1.get(x));
+            player1Score=player1Score+player1.get(x).getCardValue();
+            
+        }
+         System.out.println("player2's hand is");
+        for (int x=0; x<6;x++){
+            System.out.println(player2.get(x));
+            player2Score=player2Score+player2.get(x).getCardValue();
+            
+        }
+         System.out.println("player3's hand is");
+        for (int x=0; x<6;x++){
+            System.out.println(player3.get(x));
+            player3Score=player3Score+player3.get(x).getCardValue();
+            
+        }
+         System.out.println("player4's hand is");
+        for (int x=0; x<6;x++){
+            System.out.println(player4.get(x));
+            player4Score=player4Score+player4.get(x).getCardValue();
+            
+        }
+         System.out.println("player5's hand is");
+        for (int x=0; x<6;x++){
+            System.out.println(player5.get(x));
+            player5Score=player5Score+player5.get(x).getCardValue();
+            
+        }
+
     }
+    if (player1Score>player2Score){
+    System.out.println("Player1 beats Player2");
+}
+}
 }
--- Deck.java
+++ Deck.java
@@ -3,95 +3,144 @@
 // Software and Programming Development 11A
 //Cards Project inherited class and Deck class
 //December 1, 2023
-public class Deck  extends CardsDeck {  
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Deck{
     
     //Let's inherit the card descriptions to be able to put them into the deck
-    String cardType = cardSuit;
-    int cardSuitNumber = numberOfSuits;
-    int cards = cardsInDeck;
-    int cardsSuit = numberOfCardsInSuit;
-    String suitType = typeOfCardsInSuit;
-    String typeofCard = typeOfCardInDeck;
-    int cardsDealt = numberOfCardsDealtOverall;
-    int cardsDealtPerHand = numberOfCardsDeltToEachPersonPerHand;
-    int numberOfHands = numberOfHandsDealt;
+   ArrayList cardsInDeck;
+   int nextCard;
+   int totalNumberOfCardsInDeck=52;
+
+   public Deck (){
+    cardsInDeck = new ArrayList <Card>();
+
     
 //create the cards
-{
-    Card cardInfo1 = new int cardSuitNumber [4];
-    //how many suits there are
-    Card cardInfo2 = new int cardsSuit [13];
-    // how many cards are in a suit
-    Card cardInfo3 = new suitType ("Spades");
-    Card cardInfo3 = new suitType ("Clubs");
-    Card cardInfo3 = new suitType ("Hearts");
-    Card cardInfo3 = new suitType ("Diamonds");
-    //the different suits
-    Card cardInfo4 = new int cards [54];
-    //number of cards in the deck
-    Card cardInfo5 = new typeOfCard ("Ace of Spades");
-    Card cardInfo5 = new typeOfCard ("King of Spades");
-    Card cardInfo5 = new typeOfCard ("Queen of Spades");
-    Card cardInfo5 = new typeOfCard ("Jack of Spades");
-    Card cardInfo5 = new typeOfCard ("2 of Spades");
-    Card cardInfo5 = new typeOfCard ("3 of Spades");
-    Card cardInfo5 = new typeOfCard ("4 of Spades");
-    Card cardInfo5 = new typeOfCard ("5 of Spades");
-    Card cardInfo5 = new typeOfCard ("6 of Spades");
-    Card cardInfo5 = new typeOfCard ("7 of Spades");
-    Card cardInfo5 = new typeOfCard ("8 of Spades");
-    Card cardInfo5 = new typeOfCard ("9 of Spades");
-    Card cardInfo5 = new typeOfCard ("10 of Spades");
-// the cards in the spades suit
-    Card cardInfo5 = new typeOfCard ("Ace of Hearts");
-    Card cardInfo5 = new typeOfCard ("King of Hearts");
-    Card cardInfo5 = new typeOfCard ("Queen of Hearts");
-    Card cardInfo5 = new typeOfCard ("Jack of Hearts");
-    Card cardInfo5 = new typeOfCard ("2 of Hearts");
-    Card cardInfo5 = new typeOfCard ("3 of Hearts");
-    Card cardInfo5 = new typeOfCard ("4 of Hearts");
-    Card cardInfo5 = new typeOfCard ("5 of Hearts");
-    Card cardInfo5 = new typeOfCard ("6 of Hearts");
-    Card cardInfo5 = new typeOfCard ("7 of Hearts");
-    Card cardInfo5 = new typeOfCard ("8 of Hearts");
-    Card cardInfo5 = new typeOfCard ("9 of Hearts");
-    Card cardInfo5 = new typeOfCard ("10 of Hearts");
-    // cards in the hearts suit
-    Card cardInfo5 = new typeOfCard ("Ace of Diamonds");
-    Card cardInfo5 = new typeOfCard ("King of Diamonds");
-    Card cardInfo5 = new typeOfCard ("Queen of Diamonds");
-    Card cardInfo5 = new typeOfCard ("Jack of Diamonds");
-    Card cardInfo5 = new typeOfCard ("2 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("3 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("4 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("5 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("6 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("7 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("8 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("9 of Diamonds");
-    Card cardInfo5 = new typeOfCard ("10 of Diamonds");
-    // the cards in diamonds suit
-    Card cardInfo5 = new typeOfCard ("Ace of Clubs");
-    Card cardInfo5 = new typeOfCard ("King of Clubs");
-    Card cardInfo5 = new typeOfCard ("Queen of Clubs");
-    Card cardInfo5 = new typeOfCard ("Jack of Clubs");
-    Card cardInfo5 = new typeOfCard ("2 of Clubs");
-    Card cardInfo5 = new typeOfCard ("3 of Clubs");
-    Card cardInfo5 = new typeOfCard ("4 of Clubs");
-    Card cardInfo5 = new typeOfCard ("5 of Clubs");
-    Card cardInfo5 = new typeOfCard ("6 of Clubs");
-    Card cardInfo5 = new typeOfCard ("7 of Clubs");
-    Card cardInfo5 = new typeOfCard ("8 of Clubs");
-    Card cardInfo5 = new typeOfCard ("9 of Clubs");
-    Card cardInfo5 = new typeOfCard ("10 of Clubs");
-// cards in clubs suit
-    Card cardInfo6 = new int cardsDealt [30];
-    //cards dealt overall in all 5 hands
-    Card cardInfo7 = new int cardsDealtPerHand [6];
-    Card cartInfo8 = new int numberOfHands [5];
+
+    Card cardInfo1 = new Card(0,"Spades","Ace", 13);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","King", 12);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","Queen", 11);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","Jack", 10);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","10", 9);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","9", 8);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","8", 7);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","7", 6);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","6", 5);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","5", 4);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","4", 3);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","3", 2);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Spades","2", 1);
+    cardsInDeck.add (cardInfo1);
+
+    cardInfo1 = new Card(0,"Hearts","Ace", 13);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","King", 12);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","Queen", 11);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","Jack", 10);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","10", 9);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","9", 8);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","8", 7);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","7", 6);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","6", 5);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","5", 4);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","4", 3);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","3", 2);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Hearts","2", 1);
+    cardsInDeck.add (cardInfo1);
+
+    cardInfo1 = new Card(0,"Diamonds","Ace", 13);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","King", 12);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","Queen", 11);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","Jack", 10);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","10", 9);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","9", 8);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","8", 7);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","7", 6);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","6", 5);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","5", 4);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","4", 3);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","3", 2);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Diamonds","2", 1);
+    cardsInDeck.add (cardInfo1);
+
+    cardInfo1 = new Card(0,"Clubs","Ace", 13);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","King", 12);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubd","Queen", 11);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","Jack", 10);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","10", 9);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","9", 8);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","8", 7);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","7", 6);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","6", 5);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","5", 4);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","4", 3);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","3", 2);
+    cardsInDeck.add (cardInfo1);
+    cardInfo1 = new Card(0,"Clubs","2", 1);
+    cardsInDeck.add (cardInfo1);
+    nextCard=0;
 }
-    // let's make the deck and shuffle the cards
- {
-    
- }
+public void DeckShuffle(){
+    Collections.shuffle(cardsInDeck);
+    nextCard=0;
+}
+public Card dealNextCard(){
+return cardsInDeck.get(nextCard++);
+}
+public boolean isNextCard(){
+    if (nextCard<=cardsInDeck.size()) {
+        return true;
+    }
+    return false;
+    }
 }
