4b52e621e8ea7de45c7e202d6394b71642e0b23f
G-crafts <katelynn.graham@malad.us>
2025-03-18 20:33:04 -0600

    Create Test12.java

---
+++ Card Game/Test12.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test12{
+
+     {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Insert binary code here:");
+        
+        int userInt = scanner.nextInt( 1011000101);
+
+
+}
+}
